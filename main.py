034ab7a8ea9515d2331821f453eb18c0947a6526
ZSADfghjk <zapytaj51@gmail.com>
2022-05-03 00:16:31 +0200

    Unfinished version.
    Main.py:
    - Check permission for linux
    
    scan.py:
    -Code cleaning
    
    To do:
    - User can be apple to delete duplicates
    - GUI to select file to delete

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-
+import os
 from ctypes import windll, c_long, byref
 import sys
-import os
 import scan
 
 
 def main():
-    if not windll.shell32.IsUserAnAdmin():
-        windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-        exit(1)
 
-    if os.name == 'nt':  # Disable redirection from system32 to Syswow64 on 64 bit machines if operating system is
-        # Windows
+    if sys.platform == 'win32':  # Disable redirection from system32 to Syswow64 on 64 bit machines and check
+        # admin permission for Windows
+
+        if not windll.shell32.IsUserAnAdmin():
+            windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            exit(1)
+
         dll_hook = windll.kernel32
         wow64 = c_long(0)
         dll_hook.Wow64DisableWow64FsRedirection(byref(wow64))
+    elif sys.platform == 'linux' and os.geteuid() != 0:  # Check permission for linux
+        print('You need root privilege')
+        raise EnvironmentError
 
     scan.scan_files().to_csv('files/duplicates.csv')  # Start scan
 
--- scan.py
+++ scan.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import hashlib
-import shutil
 import multiprocessing as mp
 from sys import exc_info
 import glob
@@ -10,29 +9,15 @@ import os
 import psutil
 
 
-# def drop_uniques(datafr: pd.DataFrame, col_name: str) -> pd.DataFrame:
-# try:
-#   data = datafr.duplicated(subset=[col_name], keep=False)
-
-#  for i in data.iteritems():
-#     if not i[1]:
-#        datafr.drop(index=i[0], inplace=True)
-# except:
-#   with open('log/Scan_error.log', 'a', encoding='utf-8') as s_error:
-#      s_error.write(f'\n-------------------------------------\n')
-#     s_error.write(f'Error while saving to duplicates:{exc_info()}')
-
-# return datafr
-
 def drop_uniques(datafr: pd.DataFrame, col_name: str, keep_test_col: bool) -> pd.DataFrame:
     data = pd.DataFrame(datafr.duplicated(subset=[col_name], keep=False), columns=['isDuplicated'])  # Mark uniques
     # and duplicates based on col_name
 
     duplicates = pd.concat([datafr, data], axis=1)
 
-    duplicates = duplicates[duplicates.isDuplicated != 0]   # keep only duplicated rows
+    duplicates = duplicates[duplicates.isDuplicated != 0]  # keep only duplicated rows
 
-    if not keep_test_col:   # drop duplication test column if False
+    if not keep_test_col:  # drop duplication test column if False
         duplicates.drop('isDuplicated', axis=1, inplace=True)
 
     print(duplicates)
@@ -52,43 +37,35 @@ def get_drives() -> list:  # Get drives letters
 
 
 def parallel_scan(drive_l: str) -> pd.DataFrame:
-    total, used, free = shutil.disk_usage(drive_l)  # Get disk partition info
     df = pd.DataFrame(columns=['path', 'hash'])
 
-    # convert to Gb
-    total = round(((total / 1024) / 1024) / 1024, 2)
-    free = round(((free / 1024) / 1024) / 1024, 2)
-    used = round(((used / 1024) / 1024) / 1024, 2)
-    print(
-        f'Scanning files in {drive_l}. {used}Gb is used of total {total}Gb. {free}Gb are free')
+    print(f'Scanning files in {drive_l}.')
 
     if not os.path.isdir('log'):  # make log director if it`s not exist
         os.mkdir('log')
 
+    if not os.path.isdir('files'):  # make files director if it`s not exist
+        os.mkdir('files')
+
+    # Create log files
     start_date = datetime.now().strftime('%d.%m.%Y_%H.%M.%S')
     err_log = open(f'log/files_error_{start_date}_{drive_l[0]}.log', 'w', encoding='utf-8')  # logging all file errors
     df_info = open(f'log/Info_{start_date}_{drive_l[0]}.log', 'w', encoding='utf-8')  # File to save dataframe info
+
     df_info.write(f'Scanning has started at: {start_date} \n')
     df_info.flush()
 
     for file in glob.iglob(fr'{drive_l}\**\*.*', recursive=True):  # Scan loop
+
+        # noinspection PyBroadException
         try:
-            if not os.path.islink(os.path.dirname(file)):   # Skip symlinks
+            if not os.path.islink(os.path.dirname(file)):  # Skip symlinks
                 with open(file, 'rb') as f:
                     f_hash = hashlib.sha3_512(f.read(2048)).hexdigest()
                     df = pd.concat([df, pd.DataFrame.from_records([{'path': fr'{file}', 'hash': f_hash}])],
                                    ignore_index=True)
-        except PermissionError:
-            err_log.write(fr'{file}: Permission error:{exc_info()}' '\n')
-        except ValueError:
-            err_log.write(fr'{file}: Value error:{exc_info()}''\n')
-        except OSError:
-            err_log.write(fr'{file}: OSError:{exc_info()}''\n')
-        except UnicodeEncodeError:
-            err_log.write(fr'{file}: UnicodeEncodeError:{exc_info()}''\n')
-        except MemoryError:
-            err_log.write(fr'{file}: Memory error:{exc_info()}''\n')
-            exit(2)
+        except BaseException:
+            err_log.write(fr'{file}: {exc_info()}' '\n')
 
     end_date = datetime.now().strftime('%d.%m.%Y_%H.%M.%S.%f')
     buffer = StringIO()
@@ -96,8 +73,8 @@ def parallel_scan(drive_l: str) -> pd.DataFrame:
     df_info.write(buffer.getvalue() + '\n' + 'Scanning has been completed at: ' + end_date)  # write end date to file
     df_info.flush()
 
-    df.to_csv(fr'files\{drive_l[0]}.csv')  # write paths and hash from one partition to csv(only for debug. this may be
-    # deleted in final version)
+    df.to_csv(fr'files\{drive_l[0]}.csv')  # write paths and hashes from one partition to csv(only for debug. this
+    # may be deleted in final version)
 
     df_info.close()
     err_log.close()
